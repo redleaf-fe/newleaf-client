7c59b66c7fa1f6ebbb660e35f6104ce7d4b17838
vzhufeng <vzhufeng@163.com>
2021-05-18 14:10:53 +0800

    feat(api): modify request

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,21 +2,20 @@ import { Message } from 'redleaf-rc';
 import axios from 'axios';
 
 const req = function ({ url, method = 'get', data = {} }) {
-  const dataMap = {
-    get: { params: data },
-    post: data,
-  };
-  return axios[method](url, {
-    // window.location.host
-    // http://localhost:3011
-    baseURL: 'http://localhost:3011',
-    ...dataMap[method],
-  })
+  return axios({
+      url,
+      method,
+      headers: { 'Content-Type': 'application/json' },
+      // window.location.host
+      // http://localhost:3011
+      baseURL: 'http://localhost:3011',
+      data,
+    })
     .then((res) => {
       if (res.status === 200 && res.statusText === 'OK') return res.data;
     })
     .catch((err) => {
-      Message.show({ content: String(err) });
+      Message.show({ content: err.response.data.message });
     });
 };
 
