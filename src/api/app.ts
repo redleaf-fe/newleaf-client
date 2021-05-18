7f29e7c44b1976d2a27c47f3047cdfac8a32cd75
vzhufeng <vzhufeng@163.com>
2021-05-18 14:40:52 +0800

    feat(api): add app

---
+++ src/api/app.ts
@@ -0,0 +1,9 @@
+import req from './request';
+
+const prefix = 'app';
+
+export const getAppList = () => req({ url: `${prefix}/list` });
+
+export const saveApp = (data) => req({ url: `${prefix}/save`, method: 'post', data });
+
+export const appDetail = (data) => req({ url: `${prefix}/detail`, data });
