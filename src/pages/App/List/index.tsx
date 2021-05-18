1cbc6986107a24e70a8d97ff82ead8861e5c4946
vzhufeng <vzhufeng@163.com>
2021-05-18 14:47:42 +0800

    feat(pages): applist dev

--- src/pages/App/List/index.tsx
+++ src/pages/App/List/index.tsx
@@ -1,5 +1,54 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Button, Table, Dialog } from 'redleaf-rc';
+import { getAppList } from '@/api/app';
+
+import CreateDlg from './createDlg';
+import './style.less';
+
+const columns = [
+  {
+    title: '应用名称',
+    columnKey: 'name',
+  },
+  {
+    title: '创建时间',
+    columnKey: 'createdAt',
+  },
+  {
+    title: '创建人',
+    columnKey: 'creator',
+  },
+];
 
 export default () => {
-  return <div>app</div>;
+  const [datasets, setDatasets] = useState([]);
+  const dlgRef = useRef();
+
+  useEffect(() => {
+    getAppList().then((res) => {
+      setDatasets(res);
+    });
+  }, []);
+
+  const closeDlg = useCallback(() => {
+    dlgRef.current?.();
+  }, []);
+
+  return (
+    <div className="applist-container">
+      <div>
+        <Button
+          onClick={() => {
+            dlgRef.current = Dialog.show({
+              content: <CreateDlg close={closeDlg} />,
+              title: '新建应用',
+            });
+          }}
+        >
+          新建应用
+        </Button>
+      </div>
+      <Table columns={columns} datasets={datasets} bordered="row" />
+    </div>
+  );
 };
