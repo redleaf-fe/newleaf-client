858c61eb1b303e3c9cdde41c7cdcb683edaa5d7f
vzhufeng <vzhufeng@163.com>
2021-05-18 14:20:40 +0800

    feat(pages): config dev

--- src/pages/Config/index.tsx
+++ src/pages/Config/index.tsx
@@ -1,42 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { Message, ConfigProvider } from 'redleaf-rc';
+import React, { useState, useEffect, useContext } from 'react';
+import { Message } from 'redleaf-rc';
 import { getDatabaseConfig } from '@/api/config';
 import LabelContent from '@/components/labelContent';
+import { context } from '@/components/layout';
 
 import './style.less';
 
 const Config = () => {
   const [config, setConfig] = useState({});
+  const layout = useContext(context);
 
   useEffect(() => {
+    layout?.setLayoutVal({ key: 'pageTitle', value: '数据库配置' });
+
     getDatabaseConfig()
       .then((res) => setConfig(res?.data || {}))
       .catch((e) => Message.show({ title: String(e) }));
   }, []);
 
   return (
-    <ConfigProvider.Consumer>
-      {(val) => {
-        val.setGlobal({ key: 'pageTitle', value: '数据库配置' });
-
-        return (
-          <div className="config-container">
-            <LabelContent
-              items={[
-                {
-                  label: '数据库名称：',
-                  value: config?.database,
-                },
-                {
-                  label: '数据库端口号：',
-                  value: config?.port,
-                },
-              ]}
-            />
-          </div>
-        );
-      }}
-    </ConfigProvider.Consumer>
+    <div className="config-container">
+      <LabelContent
+        items={[
+          {
+            label: '数据库名称：',
+            value: config?.database,
+          },
+          {
+            label: '数据库端口号：',
+            value: config?.port,
+          },
+        ]}
+      />
+    </div>
   );
 };
 
