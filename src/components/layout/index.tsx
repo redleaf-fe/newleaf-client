9ad21351d7e79a46ab6a06510bb9378c78c34723
vzhufeng <vzhufeng@163.com>
2021-05-18 14:05:14 +0800

    feat(components): add global value provider

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { ConfigProvider } from 'redleaf-rc';
 import './style.less';
 
 const Layout = (props) => {
+  const [global, setGlobal] = useState({
+    pageTitle: '',
+  });
+
+  const setGlobalVal = useCallback(
+    ({ key, value }) => {
+      if (global[key] !== value) {
+        const obj = Object.assign({}, global);
+        obj[key] = value;
+        setGlobal(obj);
+      }
+    },
+    [global],
+  );
+
   return (
-    <div className="pageContainer">
-      <div className="menuContainer">menu</div>
-      <div className="pageContent">{props.children}</div>
-    </div>
+    <ConfigProvider.Provider setGlobal={setGlobalVal}>
+      <div className="page-container">
+        <div className="menu-container">menu</div>
+        <div className="page-content">
+          <div className="page-title">{global.pageTitle}</div>
+          <div className="main">{props.children}</div>
+        </div>
+      </div>
+    </ConfigProvider.Provider>
   );
 };
 
