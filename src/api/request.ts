6223bf637a4b14e178a6758f0393aa21591a27c2
vzhufeng <vzhufeng@163.com>
2021-05-18 14:29:06 +0800

    feat(api): request add redirect logic

--- src/api/request.ts
+++ src/api/request.ts
@@ -1,22 +1,28 @@
 import { Message } from 'redleaf-rc';
 import axios from 'axios';
 
-const req = function ({ url, method = 'get', data = {} }) {
+export default function ({ url, method = 'get', data = {} }: { url: string; method: 'get' | 'post'; data: any }) {
   return axios({
-      url,
-      method,
-      headers: { 'Content-Type': 'application/json' },
-      // window.location.host
-      // http://localhost:3011
-      baseURL: 'http://localhost:3011',
-      data,
-    })
+    url,
+    method,
+    headers: { 'Content-Type': 'application/json' },
+    // window.location.host
+    // http://localhost:3011
+    baseURL: 'http://localhost:3011',
+    data,
+  })
     .then((res) => {
-      if (res.status === 200 && res.statusText === 'OK') return res.data;
+      if (res.status === 200 && res.statusText === 'OK') {
+        return res.data;
+      }
+      return res;
     })
     .catch((err) => {
-      Message.show({ content: err.response.data.message });
+      const res = err.response;
+      if ([301, 302].includes(res.status)) {
+        window.location.href = `/#${res.data.redirectUrl}`;
+      } else {
+        Message.show({ title: err.response.data.message });
+      }
     });
-};
-
-export default req;
+}
