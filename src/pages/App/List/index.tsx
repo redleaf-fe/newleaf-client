56f10cdb3eadb4ff9632f09068cea05867dae878
vzhufeng <vzhufeng@163.com>
2021-05-18 15:36:22 +0800

    feat(pages): applist dev

--- src/pages/App/List/index.tsx
+++ src/pages/App/List/index.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { Button, Table, Dialog } from 'redleaf-rc';
+import { Button, Table, Dialog, Message } from 'redleaf-rc';
 import { getAppList } from '@/api/app';
+import dayjs from 'dayjs';
 
 import CreateDlg from './createDlg';
 import './style.less';
@@ -8,15 +9,42 @@ import './style.less';
 const columns = [
   {
     title: '应用名称',
-    columnKey: 'name',
+    columnKey: 'appName',
+    width: '300px',
   },
   {
-    title: '创建时间',
-    columnKey: 'createdAt',
+    title: '最后更新时间',
+    columnKey: 'updatedAt',
+    width: '200px',
+    render({ meta }) {
+      return <div>{dayjs(meta.updatedAt).format('YYYY-MM-DD HH:mm:ss')}</div>;
+    },
   },
   {
-    title: '创建人',
-    columnKey: 'creator',
+    title: '仓库地址',
+    columnKey: 'git',
+    width: '30%',
+    render({ meta }) {
+      return <div className="gitAddress">{meta.git}</div>;
+    },
+  },
+  {
+    title: '操作',
+    columnKey: 'op',
+    width: '100px',
+    render({ meta }) {
+      return (
+        <div className="operate">
+          <span className="color-primary pointer mr8" onClick={() => {}}>
+            编辑
+          </span>
+          |
+          <span className="color-danger pointer ml8" onClick={() => {}}>
+            删除
+          </span>
+        </div>
+      );
+    },
   },
 ];
 
@@ -24,11 +52,17 @@ export default () => {
   const [datasets, setDatasets] = useState([]);
   const dlgRef = useRef();
 
-  useEffect(() => {
-    getAppList().then((res) => {
-      setDatasets(res);
-    });
+  const getList = useCallback(() => {
+    getAppList()
+      .then((res) => {
+        setDatasets(res);
+      })
+      .catch((e) => {
+        Message.show({ title: e.message });
+      });
   }, []);
+
+  useEffect(getList, [getList]);
 
   const closeDlg = useCallback(() => {
     dlgRef.current?.();
@@ -36,11 +70,11 @@ export default () => {
 
   return (
     <div className="applist-container">
-      <div>
+      <div className="mb16">
         <Button
           onClick={() => {
             dlgRef.current = Dialog.show({
-              content: <CreateDlg close={closeDlg} />,
+              content: <CreateDlg {...{ closeDlg, getList }} />,
               title: '新建应用',
             });
           }}
