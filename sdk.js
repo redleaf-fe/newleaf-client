048928af4905e12eb9da2383f21a111444fb554a
vzhufeng <vzhufeng@163.com>
2021-05-23 15:23:16 +0800

    feat: update sdk

--- sdk.js
+++ sdk.js
@@ -11,22 +11,21 @@
     this.appId = '123';
     this.logUrl = '/log';
     if (navigator.sendBeacon) {
-      this.defaultSend = 'sendBeacon';
+      this.method = 'sendBeacon';
     } else {
-      this.defaultSend = 'img';
+      this.method = 'img';
     }
   }
 
-  Newleaf.prototype.log = function ({ content = '', method, logUrl }) {
+  Newleaf.prototype.log = function ({ content = '', method, logUrl, appId }) {
     // content如果不是字符串，进行转换
     if (typeof content !== 'string') {
       content = String(content);
     }
     // 有内容才发送
     if (content) {
-      method = method || this.defaultSend;
-      logMethod[method](logUrl, {
-        appId: this.appId,
+      logMethod[method || this.method](logUrl || this.logUrl, {
+        appId: appId || this.appId,
         content,
       });
     }
@@ -42,8 +41,8 @@
   function sendXhr(url, data) {
     const xhr = new XMLHttpRequest();
     xhr.open('POST', url);
-    xhr.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
-    xhr.send(data);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
   }
 
   function sendImg(url, data) {
