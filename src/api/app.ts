27ee1ca1c1feb032208301149f067ee4d2c13b32
vzhufeng <vzhufeng@163.com>
2021-05-24 15:12:15 +0800

    feat(api): app add ids

--- src/api/app.ts
+++ src/api/app.ts
@@ -4,6 +4,8 @@ const prefix = 'app';
 
 export const getAppList = () => req({ url: `${prefix}/list` });
 
+export const getAppIDs = () => req({ url: `${prefix}/ids` });
+
 export const saveApp = (data) => req({ url: `${prefix}/save`, method: 'post', data });
 
 export const appDetail = (data) => req({ url: `${prefix}/detail`, data });
