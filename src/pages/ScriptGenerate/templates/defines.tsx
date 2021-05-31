f94e97e5a238e4b523d5fb80fdd31a25c8db186b
vzhufeng <vzhufeng@163.com>
2021-05-31 16:45:21 +0800

    feat(pages): update sdk template

--- src/pages/ScriptGenerate/templates/defines.tsx
+++ src/pages/ScriptGenerate/templates/defines.tsx
@@ -1,7 +1,23 @@
-export default `
+export default function ({ lcName }) {
+  return `
 const logMethod = {
   xhr: sendXhr,
   img: sendImg,
   beacon: sendBeacon,
 };
-`;
+
+const cache = {};
+const lcName = '${lcName || '__newleaf_log_cache__'}';
+const lc = window.localStorage || {
+  setItem: function (name, val) {
+    cache[name] = val;
+  },
+  getItem: function (name) {
+    return cache[name];
+  },
+  removeItem: function () {
+    delete cache[name];
+  }
+};
+  `;
+}
