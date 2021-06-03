b7b85eb7bbda1aba340e5f04c32c675d6682b09e
vzhufeng <vzhufeng@163.com>
2021-06-03 15:39:30 +0800

    feat(pages): app edit dev

--- src/pages/App/List/createDlg.tsx
+++ src/pages/App/List/createDlg.tsx
@@ -1,22 +1,17 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Button, Input, Form, Message } from 'redleaf-rc';
-import { saveApp, appDetail } from '@/api/app';
+import { saveApp } from '@/api/app';
 import { required, requiredMsg } from '@/utils/validators';
 import { formUnpass } from '@/const';
 
 export default (props) => {
-  const { closeDlg, getList } = props;
+  const { closeDlg, getList, info = {} } = props;
   const formRef = useRef();
-
-  // const getAppDetail = useCallback(() => {
-  //   appDetail().then((res) => {});
-  // }, []);
-
-  // useEffect(getAppDetail, [getAppDetail]);
 
   return (
     <Form
       className="create-container"
+      defaultValue={info}
       getInstance={(i) => {
         formRef.current = i;
       }}
@@ -37,10 +32,7 @@ export default (props) => {
       <Form.Item label="应用描述：" name="desc">
         <Input type="textarea" rows={5} maxLength={100} showCount />
       </Form.Item>
-      <Form.Item
-        label="仓库地址："
-        name="git"
-      >
+      <Form.Item label="仓库地址：" name="git">
         <Input type="textarea" rows={5} maxLength={200} showCount />
       </Form.Item>
       <div className="btns">
