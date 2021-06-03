9a44cd1a16e7b59845389570506b7e0ccd4987c0
vzhufeng <vzhufeng@163.com>
2021-06-03 14:02:39 +0800

    feat(pages): app list edit app

--- src/pages/App/List/index.tsx
+++ src/pages/App/List/index.tsx
@@ -1,56 +1,78 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState, useMemo } from 'react';
 import { Button, Table, Dialog, Message } from 'redleaf-rc';
-import { getAppList } from '@/api/app';
+import { getAppList, appDetail } from '@/api/app';
 import dayjs from 'dayjs';
 
 import CreateDlg from './createDlg';
 import './style.less';
 
-const columns = [
-  {
-    title: '应用名称',
-    columnKey: 'appName',
-    width: '300px',
-  },
-  {
-    title: '最后更新时间',
-    columnKey: 'updatedAt',
-    width: '200px',
-    render({ meta }) {
-      return <div>{dayjs(meta.updatedAt).format('YYYY-MM-DD HH:mm:ss')}</div>;
-    },
-  },
-  {
-    title: '仓库地址',
-    columnKey: 'git',
-    width: '30%',
-    render({ meta }) {
-      return <div className="gitAddress">{meta.git}</div>;
-    },
-  },
-  {
-    title: '操作',
-    columnKey: 'op',
-    width: '100px',
-    render({ meta }) {
-      return (
-        <div className="operate">
-          <span className="color-primary pointer mr8" onClick={() => {}}>
-            编辑
-          </span>
-          |
-          <span className="color-danger pointer ml8" onClick={() => {}}>
-            删除
-          </span>
-        </div>
-      );
-    },
-  },
-];
-
 export default () => {
   const [datasets, setDatasets] = useState([]);
   const dlgRef = useRef();
+
+  const columns = useMemo(
+    () => [
+      {
+        title: '应用名称',
+        columnKey: 'appName',
+        grow: 1,
+      },
+      {
+        title: 'appId',
+        columnKey: 'id',
+        grow: 1,
+      },
+      {
+        title: '最后更新时间',
+        columnKey: 'updatedAt',
+        width: '100px',
+        render({ meta }) {
+          return <div>{dayjs(meta.updatedAt).format('YYYY-MM-DD HH:mm:ss')}</div>;
+        },
+      },
+      {
+        title: '仓库地址',
+        columnKey: 'git',
+        width: '30%',
+        render({ meta }) {
+          return <div className="gitAddress">{meta.git}</div>;
+        },
+      },
+      {
+        title: '操作',
+        columnKey: 'op',
+        width: '100px',
+        render({ meta }) {
+          return (
+            <div className="operate">
+              <span
+                className="color-primary pointer mr8"
+                onClick={() => {
+                  appDetail({ id: meta.id })
+                    .then((res) => {
+                      dlgRef.current = Dialog.show({
+                        content: <CreateDlg {...{ closeDlg, getList, info: res }} />,
+                        title: '编辑应用',
+                      });
+                    })
+                    .catch((e) => {
+                      Message.show({ title: e.message });
+                    });
+                }}
+              >
+                编辑
+              </span>
+              |
+              <span className="color-danger pointer ml8" onClick={() => {}}>
+                删除
+              </span>
+            </div>
+          );
+        },
+      },
+    ],
+    [],
+  );
 
   const getList = useCallback(() => {
     getAppList()
