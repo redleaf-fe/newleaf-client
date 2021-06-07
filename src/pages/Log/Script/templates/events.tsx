f65c5e2c18b4800de3e693e24a3bf75c3e6eb50b
vzhufeng <vzhufeng@163.com>
2021-06-07 20:36:14 +0800

    feat(pages): log script add route

--- src/pages/Log/Script/templates/events.tsx
+++ src/pages/Log/Script/templates/events.tsx
@@ -1,4 +1,4 @@
-export default function () {
+export function error() {
   return `
   window.addEventListener('error', function (e) {
     const { message, stack } = e.error;
@@ -8,7 +8,7 @@ export default function () {
         stack
       }),
       type: 'error'
-    })
+    });
   });
 
   window.addEventListener('unhandledrejection', function (e) {
@@ -19,7 +19,44 @@ export default function () {
         stack
       }),
       type: 'error'
-    })
+    });
   });
+  `;
+}
+
+export function route() {
+  return `
+  function hackHistory(type) {
+    var source = window.history[type];
+    return function () {
+      var event = new Event(type);
+      event._args = arguments;
+      window.dispatchEvent(event);
+      var rewrite = source.apply(this, arguments);
+      return rewrite;
+    };
+  }
+
+  window.history.pushState = hackHistory('pushState');
+  window.history.replaceState = hackHistory('replaceState');
+
+  window.addEventListener('pushState', function (e) {
+    newleaf.log({
+      content: e._args[2] || '',
+      type: 'route'
+    });
+  }, true);
+  window.addEventListener('replaceState', function (e) {
+    newleaf.log({
+      content: e._args[2] || '',
+      type: 'route'
+    });
+  }, true);
+  window.addEventListener('hashchange', function (e) {
+    newleaf.log({
+      content: e.newURL,
+      type: 'route'
+    });
+  }, true);
   `;
 }
