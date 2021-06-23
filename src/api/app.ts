ef6895bf0e062b642653743cb23cee34e6d5e6fc
vzhufeng <vzhufeng@163.com>
2021-06-23 09:53:16 +0800

    feat: delete useless api

--- src/api/app.ts
+++ src/api/app.ts
@@ -6,6 +6,4 @@ export const getAppList = (data) => req({ url: `${prefix}/list`, data });
 
 export const saveApp = (data) => req({ url: `${prefix}/save`, method: 'post', data });
 
-export const deleteApp = (data) => req({ url: `${prefix}/delete`, method: 'post', data });
-
 export const appDetail = (data) => req({ url: `${prefix}/detail`, data });
--- src/api/appPublish.ts
+++ src/api/appPublish.ts
@@ -6,6 +6,4 @@ export const getPubishList = (data) => req({ url: `${prefix}/list`, data });
 
 export const savePublish = (data) => req({ url: `${prefix}/save`, method: 'post', data });
 
-export const deletePublish = (data) => req({ url: `${prefix}/delete`, method: 'post', data });
-
 export const publishDetail = (data) => req({ url: `${prefix}/detail`, data });
