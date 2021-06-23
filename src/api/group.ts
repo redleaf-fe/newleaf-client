311e98048cc1fa624d45c47232fcc088bb73eefb
vzhufeng <vzhufeng@163.com>
2021-06-23 09:54:12 +0800

    feat: add group api

---
+++ src/api/group.ts
@@ -0,0 +1,11 @@
+import req from './request';
+
+const prefix = 'group';
+
+export const getGroupList = (data) => req({ url: `${prefix}/list`, data });
+
+export const saveGroup = (data) => req({ url: `${prefix}/save`, method: 'post', data });
+
+export const saveMember = (data) => req({ url: `${prefix}/saveMember`, data });
+
+export const deleteMember = (data) => req({ url: `${prefix}/deleteMember`, data });
