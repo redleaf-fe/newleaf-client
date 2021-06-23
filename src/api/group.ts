a0476b191bc9a1bf475c5fdb85b4632d230ac751
vzhufeng <vzhufeng@163.com>
2021-06-23 14:23:19 +0800

    feat: add group detail api

--- src/api/group.ts
+++ src/api/group.ts
@@ -4,6 +4,8 @@ const prefix = 'group';
 
 export const getGroupList = (data) => req({ url: `${prefix}/list`, data });
 
+export const getGroupDetail = (data) => req({ url: `${prefix}/detail`, data });
+
 export const saveGroup = (data) => req({ url: `${prefix}/save`, method: 'post', data });
 
 export const saveMember = (data) => req({ url: `${prefix}/saveMember`, data });
