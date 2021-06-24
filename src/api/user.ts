ee7884ae7e0b02609f02657d994360d1a72f034f
vzhufeng <vzhufeng@163.com>
2021-06-24 21:17:28 +0800

    feat(api): add group and project operation into user

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,3 +3,9 @@ import req from './request';
 const prefix = 'user';
 
 export const getUserByName = (data) => req({ url: `${prefix}/getByName`, data });
+
+export const getMembersInNamespace = (data) => req({ url: `${prefix}/getMembersInNamespace`, data });
+
+export const saveUsersToNamespace = (data) => req({ url: `${prefix}/saveUsersToNamespace`, method: 'post', data });
+
+export const removeUserFromNamespace = (data) => req({ url: `${prefix}/removeUserFromNamespace`, method: 'post', data });
