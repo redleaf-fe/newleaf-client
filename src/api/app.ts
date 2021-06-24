3034bb22fd5aacbe325c46090a55647da52cdc69
vzhufeng <vzhufeng@163.com>
2021-06-24 17:03:49 +0800

    feat(api): app add method

--- src/api/app.ts
+++ src/api/app.ts
@@ -6,4 +6,8 @@ export const getAppList = (data) => req({ url: `${prefix}/list`, data });
 
 export const saveApp = (data) => req({ url: `${prefix}/save`, method: 'post', data });
 
-export const appDetail = (data) => req({ url: `${prefix}/detail`, data });
+export const getAppDetail = (data) => req({ url: `${prefix}/detail`, data });
+
+export const saveMember = (data) => req({ url: `${prefix}/saveMember`, data });
+
+export const deleteMember = (data) => req({ url: `${prefix}/deleteMember`, data });
