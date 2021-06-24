fb646847944bc5abd7ad4b04e1fcbe974043308a
vzhufeng <vzhufeng@163.com>
2021-06-24 21:04:37 +0800

    feat(api): del useless

--- src/api/userGroup.ts
+++
@@ -1,9 +0,0 @@
-import req from './request';
-
-const prefix = 'userGroup';
-
-export const getAuthList = (data) => req({ url: `${prefix}/list`, data });
-
-export const saveAuth = (data) => req({ url: `${prefix}/save`, method: 'post', data });
-
-export const deleteAuth = (data) => req({ url: `${prefix}/delete`, method: 'post', data });
