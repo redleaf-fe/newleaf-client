b7af9d1b0dd81028dd5474a4b10593f669017e9f
vzhufeng <vzhufeng@163.com>
2021-06-25 15:04:31 +0800

    fix(api): fix group method url

--- src/api/group.ts
+++ src/api/group.ts
@@ -9,8 +9,8 @@ export const getGroupDetail = (data) => req({ url: `${prefix}/detail`, data });
 export const saveGroup = (data) => req({ url: `${prefix}/save`, method: 'post', data });
 
 // 应用操作
-export const getAppInGroup = (data) => req({ url: `${prefix}/save`, data });
+export const getAppInGroup = (data) => req({ url: `${prefix}/getAppInGroup`, data });
 
-export const shareAppWithGroup = (data) => req({ url: `${prefix}/save`, method: 'post', data });
+export const shareAppWithGroup = (data) => req({ url: `${prefix}/shareAppWithGroup`, method: 'post', data });
 
-export const delShareAppWithGroup = (data) => req({ url: `${prefix}/save`, method: 'post', data });
+export const delShareAppWithGroup = (data) => req({ url: `${prefix}/delShareAppWithGroup`, method: 'post', data });
