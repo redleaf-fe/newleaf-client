71958aab9063d41ac1522b7c4d3eb3fe3bb09f9f
vzhufeng <vzhufeng@163.com>
2021-07-01 11:08:26 +0800

    feat(api): app add getAllApp method

--- src/api/app.ts
+++ src/api/app.ts
@@ -4,6 +4,8 @@ const prefix = 'app';
 
 export const getAppByName = (data) => req({ url: `${prefix}/getByName`, data });
 
+export const getAllApp = (data?) => req({ url: `${prefix}/all`, data });
+
 export const getAppList = (data) => req({ url: `${prefix}/list`, data });
 
 export const saveApp = (data) => req({ url: `${prefix}/save`, method: 'post', data });
