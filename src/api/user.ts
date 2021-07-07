1a19fb0d5747798c270bf9e5c5363fb34c692ac5
vzhufeng <vzhufeng@163.com>
2021-07-07 11:21:13 +0800

    style: dialog-side style

--- src/api/user.ts
+++ src/api/user.ts
@@ -6,6 +6,6 @@ export const getUserByName = (data) => req({ url: `${prefix}/getByName`, data })
 
 export const getMembersInNamespace = (data) => req({ url: `${prefix}/getMembersInNamespace`, data });
 
-export const saveUsersToNamespace = (data) => req({ url: `${prefix}/saveUsersToNamespace`, method: 'post', data });
+export const saveUserToNamespace = (data) => req({ url: `${prefix}/saveUserToNamespace`, method: 'post', data });
 
 export const removeUserFromNamespace = (data) => req({ url: `${prefix}/removeUserFromNamespace`, method: 'post', data });
