f0c484da588a4f88bb2edfbac6937b621e987108
vzhufeng <vzhufeng@163.com>
2021-07-15 16:26:44 +0800

    feat(api): rename publishDetail -> buildLog

--- src/api/publish.ts
+++ src/api/publish.ts
@@ -6,7 +6,7 @@ export const getPublishList = (data) => req({ url: `${prefix}/list`, data });
 
 export const savePublish = (data) => req({ url: `${prefix}/save`, method: 'post', data });
 
-export const publishDetail = (data) => req({ url: `${prefix}/detail`, data });
+export const buildLog = (data) => req({ url: `${prefix}/buildLog`, data });
 
 export const build = (data) => req({ url: `${prefix}/build`, method: 'post', data });
 
