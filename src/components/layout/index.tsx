84ce755d71e212e30a961c4a802e727b80970539
vzhufeng <vzhufeng@163.com>
2021-07-16 15:54:58 +0800

    feat(component): menu adjust router

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -7,14 +7,14 @@ import './style.less';
 export const context = React.createContext({});
 
 const menuData = [
-  { value: 'dashboard', text: '大盘' },
-  { value: 'appList', text: '应用管理' },
-  { value: 'publishList', text: '发布管理' },
-  { value: 'logList', text: '日志列表' },
-  { value: 'logScript', text: '日志脚本' },
+  { value: '/page/dashboard', text: '大盘' },
+  { value: '/page/appList', text: '应用管理' },
+  { value: '/page/publishList', text: '发布管理' },
+  { value: '/page/logList', text: '日志列表' },
+  { value: '/page/logScript', text: '日志脚本' },
 ];
 
-const noMenuItemText = { '/buildDetail': '打包详情' };
+const noMenuItemText = { '/page/buildDetail': '打包详情', '/page/approve': '审核详情' };
 
 /* eslint-disable */
 export default class Layout extends Component {
@@ -43,47 +43,51 @@ export default class Layout extends Component {
     const { pageTitle, layout } = this.state;
     const { location, history, children } = this.props;
 
-    return (
-      <context.Provider value={{ setLayoutVal: this.setLayoutVal, layout }}>
-        <div className="page-container">
-          <div className="menu-container">
-            <div className="title">newleaf</div>
-            <Menu
-              defaultValue={location.pathname.slice(1)}
-              options={menuData}
-              onChange={({ meta }) => {
-                meta.value && history.push(`/${meta.value}`);
-                this.setState({ pageTitle: meta.text || noMenuItemText[location.pathname] });
-              }}
-            />
-          </div>
-          <div className="page-content">
-            <div className="page-title">
-              {/* 页面名称 */}
-              <span className="page-name">{pageTitle}</span>
-              {/* 账号信息 */}
-              <Trigger
-                className="user-name"
-                content={
-                  <Bubble
-                    position="topRight"
-                    className="change-user"
-                    onClick={() => {
-                      history.push(`/login`);
-                    }}
-                  >
-                    切换账号
-                  </Bubble>
-                }
-                position="bottomRight"
-              >
-                <span>{Cookie.get('username')}</span>
-              </Trigger>
+    if (location.pathname === '/page/login') {
+      return children;
+    } else {
+      return (
+        <context.Provider value={{ setLayoutVal: this.setLayoutVal, layout }}>
+          <div className="page-container">
+            <div className="menu-container">
+              <div className="title">newleaf</div>
+              <Menu
+                defaultValue={location.pathname}
+                options={menuData}
+                onChange={({ meta }) => {
+                  meta.value && history.push(meta.value);
+                  this.setState({ pageTitle: meta.text || noMenuItemText[location.pathname] });
+                }}
+              />
             </div>
-            <div className="main">{children}</div>
+            <div className="page-content">
+              <div className="page-title">
+                {/* 页面名称 */}
+                <span className="page-name">{pageTitle}</span>
+                {/* 账号信息 */}
+                <Trigger
+                  className="user-name"
+                  content={
+                    <Bubble
+                      position="topRight"
+                      className="change-user"
+                      onClick={() => {
+                        history.push(`/login`);
+                      }}
+                    >
+                      切换账号
+                    </Bubble>
+                  }
+                  position="bottomRight"
+                >
+                  <span>{Cookie.get('username')}</span>
+                </Trigger>
+              </div>
+              <div className="main">{children}</div>
+            </div>
           </div>
-        </div>
-      </context.Provider>
-    );
+        </context.Provider>
+      );
+    }
   }
 }
