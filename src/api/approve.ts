e8703034dc19c9d1aa5235798c69200cf0047f78
vzhufeng <vzhufeng@163.com>
2021-07-16 15:51:55 +0800

    feat(api): add getIns and saveIns method

--- src/api/approve.ts
+++ src/api/approve.ts
@@ -6,3 +6,6 @@ export const saveProto = (data) => req({ url: `${prefix}/saveProto`, method: 'po
 
 export const getProto = (data) => req({ url: `${prefix}/getProto`, data });
 
+export const saveIns = (data) => req({ url: `${prefix}/saveIns`, method: 'post', data });
+
+export const getIns = (data) => req({ url: `${prefix}/getIns`, data });
